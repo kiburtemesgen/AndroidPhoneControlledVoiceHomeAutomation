e5fa387331d8eb64ecdd7e6aa95bad1a71b82472
kiburtemesgen <92824995+kiburtemesgen@users.noreply.github.com>
2021-10-23 09:52:17 -0700

    Add files via upload

---
+++ Arduino_Code/sketch_oct01a/sketch_oct01a.ino
@@ -0,0 +1,29 @@
+#include <EEPROM.h>
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(11, 10);
+String voice;
+int lamp=6;
+int fan=7;
+void setup() {
+pinMode(lamp,OUTPUT);
+pinMode(fan,OUTPUT);
+}
+
+void loop() {
+while(mySerial.available()){
+delay(3);
+char c = mySerial.read();
+voice+=c;}
+if(voice.length() >0) {
+Serial.println(voice);
+if(voice=="light on")
+{digitalWrite(lamp,HIGH);}
+else if(voice=="light off")
+{digitalWrite(lamp,LOW);}
+else if(voice=="fan on")
+{digitalWrite(fan,HIGH);}
+else if(voice=="fan off")
+{digitalWrite(fan,LOW);}
+voice="";
+}
+}
